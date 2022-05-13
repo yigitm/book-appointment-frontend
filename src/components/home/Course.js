76fd9d7b527ba368c5274f57dfcfa34fef79561a
Yigit Mersin <yigitmersin@gmail.com>
2022-05-13 18:49:21 +0300

    Add min-width to course

--- src/components/home/Course.js
+++ src/components/home/Course.js
@@ -3,16 +3,17 @@ import imageDisplay from '../helper/imageDisplay';
 import Icons from '../reusables/Icons';
 import CourseText from '../reusables/CourseText';
 
-const Course = ({ courses }) => (courses.length > 0
-  ? courses.map((item) => (
-    <div
-      key={item.id}
-      className="mx-20 cursor-pointer hover:scale-105 ease-in-out duration-300"
-    >
-      {imageDisplay(item)}
-      <CourseText singleCourse={item} />
-      <Icons />
-    </div>
-  ))
-  : null);
+const Course = ({ courses }) =>
+  courses.length > 0
+    ? courses.map((item) => (
+        <div
+          key={item.id}
+          className="w-[1200px] mx-20 cursor-pointer hover:scale-105 ease-in-out duration-300"
+        >
+          {imageDisplay(item)}
+          <CourseText singleCourse={item} />
+          <Icons />
+        </div>
+      ))
+    : null;
 export default Course;
