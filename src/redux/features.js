eb28d1a072f1528c235444d4121dd4d1f8c12ef3
Yigit Mersin <yigitmersin@gmail.com>
2022-10-19 19:53:31 +0300

    update api link

--- src/redux/features.js
+++ src/redux/features.js
@@ -10,9 +10,9 @@ const loginURL = 'http://localhost:3000/api/v1/login';
 const courseURL = 'http://localhost:3000/api/v1/courses';
 */
 
-const baseURL = 'https://afternoon-ravine-99760.herokuapp.com/api/v1/users';
-const loginURL = 'https://afternoon-ravine-99760.herokuapp.com/api/v1/login';
-const courseURL = 'https://afternoon-ravine-99760.herokuapp.com/api/v1/courses';
+const baseURL = 'https://dark-flower-106.fly.dev/api/v1/users';
+const loginURL = 'https://dark-flower-106.fly.dev/api/v1/login';
+const courseURL = 'https://dark-flower-106.fly.dev/api/v1/courses';
 const delURL = `${courseURL}/delete`;
 
 const initialState = [];
